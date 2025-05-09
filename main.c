15656a5c4116c8e1aec09e5130d149b85232bbea
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2025-05-09 07:34:52 +0200

    inl finished

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
-
-// This is where you add the two functions
+float fahrenheit_to_celsius(float farenheit)
+{
+    float c = (farenheit-32.0) *5.0/9.0;
+    return c;
+}
+float celsius_to_fahrenheit(float celcius)
+{
+    float f = (celcius*9.0) / 5.0 +32.0;
+    return f;
+}
 
 int main() {
     float temp_f = 100.0;
